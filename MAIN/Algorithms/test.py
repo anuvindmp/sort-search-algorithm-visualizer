d8bc11ce9250ed109978f23b7bcb83b6957d4301
Anuvind MP <anuvindmp292003@gmail.com>
2024-01-20 22:44:32 +0530

    selection sort

--- MAIN/Algorithms/test.py
+++ MAIN/Algorithms/test.py
@@ -1,4 +1,24 @@
+def SelectionSort(nums):
+    """
+    The algorithm repeatedly selects the smallest (or largest, depending on the order desired) element from the 
+    unsorted sub-list and swaps it with the first element of the unsorted sub-list. This process is repeated until 
+    the entire list is sorted.
+    1.Start with the entire list considered as an unsorted sub-list.
+    2.Find the smallest (or largest) element in the unsorted sub-list.
+    3.Swap the found element with the first element of the unsorted sub-list.
+    4.Move the boundary between the sorted and unsorted sub-lists one element to the right.
+    5.Repeat steps 2-4 until the entire list is sorted
+
+    Time Complexity : O(n^2)
+    """
+    for i in range(len(nums)-1):
+        minPos = i  
+        for j in range (i, len(nums)):
+            if nums[j] < nums[minPos]:
+                nums[j],nums[minPos] = nums[minPos], nums[j]
+    return nums
 
 
-n = [2,6,2,8,1,6,9,3]
-print((n))
+
+n = [5,3,8,6,7,2]
+print(SelectionSort(n))
