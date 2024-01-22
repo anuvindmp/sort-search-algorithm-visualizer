8926971bddfb33b7138643cb138aac794b43e268
haribn17 <150421317+haribn17@users.noreply.github.com>
2024-01-23 00:34:36 +0530

    mainpage done
    main page done

--- MAIN/Visualization/MainPage.py
+++ MAIN/Visualization/MainPage.py
@@ -23,7 +23,7 @@ def animated_text_display(word):
 
         # Calculate the position to center the text on the screen
         x = (width - text_surface.get_width()) // 2
-        y = (height - text_surface.get_height()) // 8  # Adjusted to place it a bit more above
+        y = (height - text_surface.get_height()) // 20  # Adjusted to place it just below the top
 
         # Draw the text on the screen
         screen.blit(text_surface, (x, y))
@@ -34,12 +34,13 @@ def animated_text_display(word):
         # Pause for a short duration
         pygame.time.delay(100)  # You can adjust the delay to control the speed
 
-# Set up font
-font = pygame.font.Font(None, 83)  # Adjusted font size
+# Set up font for Welcome text
+font_path = "C:\\Users\\Hari\\Documents\\sort-search-algorithm-visualizer\\MAIN\\Assets\\fonts\\static\\RobotoMono-Bold.ttf"
+font = pygame.font.Font(font_path, 75)  # Adjusted font size
 text_color = (255, 255, 255)  # Set font color to black
 
 # Load the background image
-image_path = "C:\\Users\\Hari\\Documents\\sort-search-algorithm-visualizer\\MAIN\\Visualization\\fallascii.jpg"
+image_path = "MAIN/Visualization/comascii.jpg"
 background_image = pygame.image.load(image_path)  # Replace with the actual image file path
 background_image = pygame.transform.scale(background_image, (width, height))
 
@@ -48,15 +49,18 @@ screen.blit(background_image, (0, 0))
 pygame.display.flip()
 
 # Your word to display
-word_to_display = "Welcome"
+word_to_display = "Welcome To Algorithm Visualizer"
 
 # Call the animated_text_display function with the word
 animated_text_display(word_to_display)
 
-# Show the message below the image
-message_font = pygame.font.Font(None, 36)
+# Set up font for message
+message_font_path = "C:\\Users\\Hari\\Documents\\sort-search-algorithm-visualizer\\MAIN\\Assets\\fonts\\static\\RobotoMono-Bold.ttf"
+message_font = pygame.font.Font(message_font_path, 36)
 message_color = (255, 255, 255)
-message_text = "Click on the image to continue"
+
+# Your message to display below the image
+message_text = "Explore the World of Algorithms On A Click!"
 message_surface = message_font.render(message_text, True, message_color)
 
 message_rect = message_surface.get_rect()
