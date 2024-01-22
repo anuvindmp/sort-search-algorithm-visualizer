c2420ef0bc244733928776410897d7f3f0631a08
harishrsk55 <harishrsk55@gmail.com>
2024-01-22 21:35:06 +0530

    Update example.py

--- test and examples/pygame/example.py
+++ test and examples/pygame/example.py
@@ -5,8 +5,8 @@ pygame.init()
 screen = pygame.display.set_mode((1000, 600))
 clock = pygame.time.Clock()
 pygame.display.set_caption('TJ CREAM')
-icon = pygame.image.load('test and examples/pygame/icon.png')
-pygame.display.set_icon(icon)
+# icon = pygame.image.load('test and examples/pygame/icon.png')
+# pygame.display.set_icon(icon)
 
 while True:
     for event in pygame.event.get():
